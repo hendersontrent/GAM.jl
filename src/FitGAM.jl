226e528e059350e24cce89a30eecbe060acae2dc
Trent <trent.henderson1@outlook.com>
2023-07-15 12:42:41 +1000

    small naming fix

--- src/FitGAM.jl
+++ src/FitGAM.jl
@@ -1,26 +1,26 @@
 """
-    FitGAM(formula, data; family, link, optimizer)
+    FitGAM(ModelFormula, data; family, link, optimizer)
 Computes a basic generalized additive model (GAM) on input data. An intercept is added by default.
 Usage:
 ```julia-repl
 FitGAM(formula, data; family, link, optimizer)
 ```
 Arguments:
-- `formula` : `String` containing the expression of the model. Continuous covariates are wrapped in s() like `mgcv` in R, where `s()` has 3 parts: name of column, `k`` (integer denoting number of knots), and `degree` (polynomial degree of the spline). An example expression is `"Y ~ s(MPG, k=5, degree=3) + WHT + s(TRL, k=5, degree=2)"`
+- `ModelFormula` : `String` containing the expression of the model. Continuous covariates are wrapped in s() like `mgcv` in R, where `s()` has 3 parts: name of column, `k`` (integer denoting number of knots), and `degree` (polynomial degree of the spline). An example expression is `"Y ~ s(MPG, k=5, degree=3) + WHT + s(TRL, k=5, degree=2)"`
 - `data` : `DataFrame` containing the covariates and response variable to use.
 - `family` : `Distribution` denoting the likelihood to use. Must be one of the options in `GLM.jl`. Defaults to `Normal()`.
 - `link` : denoting the link function to use for `family`. Defaults to the canonical link of `family`.
 - `optimizer` : `Optim.jl` optimizer to use. Defaults to `GradientDescent()`. Other common choices might be `BFGS()` or `LBFGS()`.
 """
 
-function FitGAM(formula::String, data::DataFrame; family=Normal(), link=canonicallink(family), optimizer=GradientDescent())
+function FitGAM(ModelFormula::String, data::DataFrame; family=Normal(), link=canonicallink(family), optimizer=GradientDescent())
 
     # Add a column of ones to the dataframe for the intercept term and add to formula
 
     intercept = DataFrame(Intercept = ones(size(data, 1)))
     data = hcat(intercept, data)
-    formula = split(formula, " ~ ")[1] * " ~ :Intercept + " * split(formula, " ~ ")[2]
-    GAMForm = ParseFormula(formula)
+    ModelFormula = split(ModelFormula, " ~ ")[1] * " ~ :Intercept + " * split(ModelFormula, " ~ ")[2]
+    GAMForm = ParseFormula(ModelFormula)
     y = data[!, GAMForm.y]
 
     #---------------- Do spline operations for each covariate ---------------
@@ -29,7 +29,7 @@ function FitGAM(formula::String, data::DataFrame; family=Normal(), link=canonica
 
     for i in 1:nrow(GAMForm.covariates)
 
-        if GAMFom.covariates[i, 4] == true
+        if GAMForm.covariates[i, 4] == true
 
             # Extract spline components
 
@@ -91,10 +91,13 @@ function FitGAM(formula::String, data::DataFrame; family=Normal(), link=canonica
 
     #---------------- Compute final GAM ---------------
 
-    # NEED TO CREATE THE `model` OBJECT HERE FOR BELOW DOING THE ACTUAL ADDITIVE PROCESS
+    # Compute actual additive process
+
+    covariateFits.
+    model.Spline_opt.(model.x)
 
     # Return final object
 
-    outs = GAMModel(formula, data, model, covariateFits)
+    outs = GAMModel(ModelFormula, data, model, covariateFits)
     return(outs)
 end
--- src/GAMModel.jl
+++ src/GAMModel.jl
@@ -1,19 +1,19 @@
 """
-    GAMModel(formula, data, model, covariateFits)
+    GAMModel(ModelFormula, data, model, covariateFits)
 Holds information relevant to the final fitted GAM model.
 
 Usage:
 ```julia-repl
-GAMModel(formula, data, model, covariateFits)
+GAMModel(ModelFormula, data, model, covariateFits)
 ```
 Arguments:
-- `formula` : `String` containing the expression of the model.
+- `ModelFormula` : `String` containing the expression of the model.
 - `data` : `DataFrame` containing the covariates and response variable to use.
 - `model` : `Struct` containing the final generalised additive model.
 - `covariateFits` : `Union{SmoothData, NoSmoothData}` containing the coefficient, spline, and penalty information for each covariate.
 """
 struct GAMModel
-    formula::String
+    ModelFormula::String
     data::DataFrame
     model
     covariateFits::Union{SmoothData, NoSmoothData}
